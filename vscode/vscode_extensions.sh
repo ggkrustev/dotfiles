5cee34a3216a6739393d192120914e314ae2cd61
Georgi Krustev <ggkrustev@yahoo.com>
2019-06-15 19:18:01 +0300

    feat(vscode): update extensions list

--- vscode/vscode_extensions.sh
+++ vscode/vscode_extensions.sh
@@ -1,9 +1,22 @@
 declare -a extensions
 
 extensions=(
-  esbenp.prettier-vscode
-  ms-vscode.csharp
-  PeterJausovec.vscode-docker
-  tintoy.msbuild-project-tools
-  vscodevim.vim
+    esbenp.prettier-vscode
+    EditorConfig.EditorConfig
+    ms-azure-devops.azure-pipelines
+    ms-azuretools.vscode-azureappservice
+    ms-azuretools.vscode-azurefunctions
+    ms-azuretools.vscode-azurestorage
+    ms-azuretools.vscode-cosmosdb
+    ms-mssql.mssql
+    ms-vscode.azure-account
+    ms-vscode.azurecli
+    ms-vscode.csharp
+    ms-vscode.vscode-node-azure-pack
+    ms-vsts.team
+    msazurermtools.azurerm-vscode-tools
+    tintoy.msbuild-project-tools
+    PeterJausovec.vscode-docker
+    vsciot-vscode.azure-iot-toolkit
+    vscodevim.vim
 )
